c6b026ccd84d20002bd49e72420ddcf508dbdf0f
ashisah <47245623+ashisah@users.noreply.github.com>
2022-02-10 10:51:54 -0500

    Delete SalesSupervisor.java

--- SalesSupervisor.java
+++
@@ -1,6 +0,0 @@
-public class SalesSupervisor extends SalesEmployee{
-  
-  public SalesSupervisor(String firstName, String lastName, String number, double salary, double commission){
-    super(firstName, lastName, number, salary, commission);
-  }
-}
