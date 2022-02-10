0dfe5cafe4c06c6919673d5be59d8db0f6e65779
ashisah <47245623+ashisah@users.noreply.github.com>
2022-02-10 10:52:13 -0500

    Delete SalariedSupervisor.java

--- SalariedSupervisor.java
+++
@@ -1,7 +0,0 @@
-public class SalariedSupervisor extends SalariedEmployee{
-  
-  public SalariedSupervisor(String firstName, String lastName, String number, double salary){
-    super(firstName, lastName, number, salary);
-  }
-  
-}
