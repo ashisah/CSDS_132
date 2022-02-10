73132c7900e82ffd5442a15272c99afe6285f762
ashisah <47245623+ashisah@users.noreply.github.com>
2022-02-10 10:53:13 -0500

    Delete HourlySupervisor.java

--- HourlySupervisor.java
+++
@@ -1,6 +0,0 @@
-public class HourlySupervisor extends HourlyEmployee{
-  
-  public HourlySupervisor(String firstName, String lastName, String number, double hourlyRate){
-    super(firstName, lastName, number, hourlyRate);
-  }
-}
